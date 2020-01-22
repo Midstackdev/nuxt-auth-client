2a742402d8344c43048b6844c0b9a9c1428d5fc7
Midstackdev <midstackdev@gmail.com>
2020-01-22 19:06:26 +0000

    Set up

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+     Home
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+    //
+  }
+}
+</script>
+
+<style>
+
+</style>
